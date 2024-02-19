905e8dd0e97725df453f10dba899a87f3ab970cb
TranDuongThieu <thieu098dm3@gmail.com>
2024-02-19 12:11:27 +0700

    Config Firebase,  login, signup with Email and password

---
+++ app/src/main/java/com/hcmute/socialnetwork/activity/authen/LoginActivity.java
@@ -0,0 +1,70 @@
+package com.hcmute.socialnetwork.activity.authen;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.hcmute.socialnetwork.R;
+import com.hcmute.socialnetwork.activity.MainActivity;
+import com.hcmute.socialnetwork.helper.Validate;
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView registerBtn = findViewById(R.id.register);
+        Button loginBtn = findViewById(R.id.loginBtn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+                finish();
+            }
+        });
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText emailEditText = findViewById(R.id.email);
+                EditText passwordEditText = findViewById(R.id.password);
+                String email = emailEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+
+                if (!Validate.isEmailValid(email)) {
+                    Toast.makeText(LoginActivity.this, "Invalid email address", Toast.LENGTH_SHORT).show();
+                } else if (!Validate.isPasswordValid(password)) {
+                    Toast.makeText(LoginActivity.this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+                } else {
+                    auth = FirebaseAuth.getInstance();
+                    auth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (!task.isSuccessful()) {
+                                        Toast.makeText(LoginActivity.this, "Login Fail", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+    }
+}
