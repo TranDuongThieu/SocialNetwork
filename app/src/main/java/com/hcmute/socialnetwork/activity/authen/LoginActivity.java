7d462fdd517930c40f0f166e50e570c863f2e523
Ngọc Thương <waytnt17523@gmail.com>
2024-03-09 07:19:37 +0700

    Full BE FE ___ Login, register, fgPass

--- app/src/main/java/com/hcmute/socialnetwork/activity/authen/LoginActivity.java
+++ app/src/main/java/com/hcmute/socialnetwork/activity/authen/LoginActivity.java
@@ -33,7 +33,6 @@ import com.hcmute.socialnetwork.helper.Validate;
 public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth auth;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,11 +42,16 @@ public class LoginActivity extends AppCompatActivity {
         // Tham chiếu Id
         Button registerBtn = findViewById(R.id.btnRegister);
         Button loginBtn = findViewById(R.id.btnLogin);
+        Button btnForgotpassword = findViewById(R.id.btnForgotpassword);
+        EditText emailOrPhoneEditText = findViewById(R.id.edtLoginEmailorPhone);
+
+
+
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
-                //finish();
+                finish();
             }
         });
 
@@ -58,9 +62,8 @@ public class LoginActivity extends AppCompatActivity {
                 EditText passwordEditText = findViewById(R.id.edtLoginPass);
                 String emailOrPhone = emailOrPhoneEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
-
                 if (emailOrPhone.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(LoginActivity.this, "Vui lòng nhập địa chỉ email hoặc số điện thoại và mật khẩu", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Vui lòng nhập đầy đủ thông tin của bạn", Toast.LENGTH_SHORT).show();
                 } else {
                     // Kiểm tra xem người dùng đã nhập số điện thoại hay địa chỉ email
                     if (isPhoneNumberValid(emailOrPhone)) {
@@ -76,6 +79,17 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+
+        btnForgotpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fgPass = "1111111";
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                intent.putExtra("fgPass", fgPass);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     private void signInWithPhoneNumber(String emailOrPhone, String passWord) {
@@ -89,26 +103,22 @@ public class LoginActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             for (DocumentSnapshot document : task.getResult()) {
-                                Log.d(TAG, document.getId() + " => " + document.getData());
                                 String phone = document.getString("phone");
                                 String pass = document.getString("password");
-                                Log.d(TAG, "Document" + phone);
-                                Log.d(TAG, "finalEmailOrPhone" + emailOrPhone);
-                                Log.d(TAG, "pass" + pass);
                                 if (phone.equals(emailOrPhone) && pass.equals(passWord)) {
-                                    // Nếu tìm thấy sự khớp, thực hiện hành động tương ứng ở đây
-//                                    Log.d(TAG, "Match found for emailOrPhone: " + emailOrPhone);
-                                    // Ví dụ: Gọi hàm xử lý đăng nhập
+                                    Toast.makeText(LoginActivity.this,"Đăng nhập thành công",Toast.LENGTH_SHORT).show();
                                     startActivity(new Intent(LoginActivity.this, MainActivity.class));
-                                    return; // Kết thúc vòng lặp khi đã tìm thấy sự khớp
+                                    finish();
+                                    return;
                                 }
                             }
+                            Toast.makeText(LoginActivity.this,"Kiểm tra tài khoản hoặc mật khẩu",Toast.LENGTH_SHORT).show();
                         } else {
-                            Log.w(TAG, "Error getting documents.", task.getException());
+                            Exception e = task.getException();
+                            Log.d(TAG, "error"+e.toString());
                         }
                     }
                 });
-
     }
 
     public void signInWithEmailAndPassword(String email, String password) {
@@ -116,9 +126,9 @@ public class LoginActivity extends AppCompatActivity {
                 .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (!task.isSuccessful()) {
+                        if (!task.isSuccessful())
                             Toast.makeText(LoginActivity.this, "Đăng nhập không thành công", Toast.LENGTH_SHORT).show();
-                        } else {
+                        else {
                             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                             startActivity(intent);
                             finish();
@@ -130,4 +140,6 @@ public class LoginActivity extends AppCompatActivity {
         // Kiểm tra số điện thoại có chứa 10 hoặc 11 ký tự và chỉ chứa các chữ số không
         return phoneNumber.matches("^\\d{10,11}$");
     }
+
+
 }
